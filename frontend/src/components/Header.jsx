e85a3777ec8157020f8c0a71e4b95efdc0f9476d
oumaimasif <sifoumaima2016@gmail.com>
2025-03-19 03:35:17 +0000

    ajouter le responsive et change le logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,248 +1,124 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { MdMenu, MdClose } from "react-icons/md"; 
+import { Link, useLocation } from "react-router-dom";
+import { MdMenu, MdClose } from "react-icons/md";
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
 
     return (
-        <nav className="bg-white z-50 fixed w-full flex justify-around py-2 gap-8 items-center shadow-custom ">
+        <nav className="bg-white z-50 fixed w-full flex justify-around py-6 gap-8 items-center shadow-custom ">
             <Link to="/">
-                <img src="/images/Noorwayslogo2.png" alt="NoorWays Logo" className="h-20" />
+                <img src="/images/nlog.png" alt="NoorWays Logo" className="w-52" />
             </Link>
 
-            <ul className="hidden md:flex space-x-9">
+            {/* Menu Prinsipale */}
+            <ul className="hidden md:flex space-x-4 lg:space-x-9">
                 <li className="relative group">
-                    <Link to="/" className="text-gray-700 text-2xl font-semibold px-1 py-2 ">
+                    <Link to="/" className={`text-xl lg:text-2xl font-semibold px-1 py-2 ${location.pathname === "/" ? "text-purple-700" : "text-gray-700 hover-text-purple-700"}`}>
                         Accueil
-                        <span className="absolute -left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-50 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
-                    </Link>
-                </li>
-                {/* <li >
-                    <Link to="/" className="text-gray-700 text-2xl font-semibold px-3  hover:py-2  hover:bg-[#db3de033] duration-300 rounded-lg">
-                        Accueil
-                    </Link>
-                    <div className="border-b-4 w-7 border-yellow-200 "></div>
-                </li> */}
-                {/* <li>
-                    <Link to="/" className="text-gray-700 text-2xl font-semibold px-3 py-2 hover:bg-[#db3de033] duration-300 rounded-lg">
-                        Accueil
-                    </Link>
-                </li> */}
-                <li className="relative group">
-                    <Link to="/association" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
-                        Associations
-                        <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
+                        <span className={`absolute -left-0 top-9 w-full  h-1 bg-[#f7932e] transition-transform duration-200 rounded-full origin-left ${location.pathname === "/" ? "scale-x-100" : "scale-x-50 group-hover:scale-x-100"} `}></span>
+
 
                     </Link>
                 </li>
                 <li className="relative group">
-                    <Link to="/benevole" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
+                    <Link to="/association" className={`text-xl lg:text-2xl font-semibold  py-2 ${location.pathname === "/association" ? "text-purple-700" : "text-gray-700 hover-text-purple-700"}`}>
+                        Association
+                        <span className={`absolute left-0 top-9 w-full  h-1 bg-[#f7932e] 
+                          transition-transform duration-200 rounded-full origin-left
+                          ${location.pathname === "/association" ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"} `}></span>
+
+                    </Link>
+                </li>
+                <li className="relative group">
+                    <Link to="/benevole" className={`text-xl lg:text-2xl font-semibold  py-2 ${location.pathname === "/benevole" ? "text-purple-700" : "text-gray-700 hover-text-purple-700"}`}>
                         Bénévoles
-                        <span className="absolute left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
+                        <span className={`absolute left-0 top-9 w-full  h-1 bg-[#f7932e] 
+                          transition-transform duration-200 rounded-full origin-left
+                          ${location.pathname === "/benevole" ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"} `}></span>
 
                     </Link>
-                </li>
+                </li >
                 <li className="relative group">
-                    <Link to="/particulier" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
-
+                    <Link to="/particulier" className={`text-xl lg:text-2xl font-semibold  py-2 ${location.pathname === "/particulier" ? "text-purple-700" : "text - gray - 700 hover-text-purple-700"}`}>
                         Particuliers
-                        <span className="absolute left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
+                        <span className={`absolute left-0 top-9 w-full  h-1 bg-[#f7932e] 
+                          transition-transform duration-200 rounded-full origin-left
+                          ${location.pathname === "/particulier" ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"} `}></span>
 
                     </Link>
-                </li>
+                </li >
 
-            </ul>
+            </ul >
+
 
             <div className="md:ml-4 hidden md:block">
                 <Link to="/login" className="bg-purple-700 text-white px-4 py-2 text-xl font-bold rounded-full hover:bg-[#872078] duration-500">
                     Connexion
                 </Link>
             </div>
-            {/* Munu en responsive */}
-            <button 
-                onClick={() => setIsOpen(!isOpen)} 
-                className="md:hidden text-gray-700 text-3xl focus:outline-none"
+
+
+          <div>
+              {/* btn icone menu de mobile */}
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="md:hidden absolute top-5 right-5 z-50 text-gray-700 text-3xl focus:outline-none"
             >
-                {isOpen ? <MdClose /> : <MdMenu />}
-            </button>
+{isOpen ? (
+                        <MdClose className="h-8 w-8 text-purple-700" />
+                    ) : (
+                        <MdMenu className="h-8 w-8" />
+                    )}            </button>
 
-            {isOpen && (
-                <div className="absolute top-24 left-0 w-full bg-[#872078] rounded-b-2xl shadow-md md:hidden z-40">
-                    <ul className="flex flex-col space-y-4 p-4 ">
-                        <li >
-                            <Link to="/" className="block text-lg font-semibold py-2 text-white hover:text-[#f7932e]" onClick={() => setIsOpen(false)}>
-                             Accueil
-                            </Link>
-                        </li>
-                         <li>
-                         <Link to="/associations" className="block text-lg font-semibold py-2 text-white hover:text-[#f7932e]"
-                          onClick={() => setIsOpen(false)}> Associations
-                          </Link>
-                          </li>
-                          <li>
-                         <Link to="/benevole" className="block text-lg font-semibold py-2 text-white hover:text-[#f7932e]"
-                          onClick={() => setIsOpen(false)}> Bénévoles
-                          </Link>
-                          </li>
-                          <li>
-                          <Link to="/particulier" className="block text-lg font-semibold py-2 text-white hover:text-[#f7932e]"
-                          onClick={() => setIsOpen(false)}> Particuliers
-                          </Link>
-                          </li>
+            {/*menu responsive */}
 
-                        <li>
-                            <Link to="/login" 
-                                className="block text-center bg-[#18b710] text-white px-5 py-3 text-lg font-bold rounded-full hover:bg-green-500 duration-500"
-                                onClick={() => setIsOpen(false)}>
+            {
+                isOpen && (
+                    <div className="absolute w-4/5 top-1 pt-2 -mt-1 sm:top-24 right-0  h-svh bg-white rounded-b-2xl shadow-lg md:hidden z-40">
+                                  
+                        <ul className="flex flex-col p-4 ">
+                            <li className="border-b border-purple-500 py-2">
+                                <Link to="/" className={`block text-lg font-semibold py-2 ${location.pathname === "/" ? "text-[#f7932e]" : "text-purple-700 hover:text-[#f7932e]"}`} onClick={() => setIsOpen(false)}>
+                                    Accueil
+                                </Link>
+                            </li>
+                            <li className="border-b border-purple-500 py-2">
+                                <Link to="/associations" className={`block text-lg font-semibold py-2 ${location.pathname === "/associations" ? "text-[#f7932e]" : "text-purple-700 hover:text-[#f7932e]"}`}
+                                    onClick={() => setIsOpen(false)}> Associations
+                                </Link>
+                            </li>
+                            <li className="border-b border-purple-500 py-2">
+                                <Link to="/benevole" className={`block text-lg font-semibold py-2 ${location.pathname === "/benevole" ? "text-[#f7932e]" : "text-purple-700 hover:text-[#f7932e]"}`}
+                                    onClick={() => setIsOpen(false)}> Bénévoles
+                                </Link>
+                            </li>
+                            <li className="border-b border-purple-500 py-2">
+                                <Link to="/particulier" className={`block text-lg font-semibold py-2 ${location.pathname === "/particulier" ? "text-[#f7932e]" : "text-purple-700 hover:text-[#f7932e]"}`}
+                                    onClick={() => setIsOpen(false)}> Particuliers
+                                </Link>
+                            </li>
+
+                            {/* //! Annonce partie */}
+
+                            <li className=" p-6 border-t ">
+                            <Link 
+                                to="/login"
+                                className="block w-full bg-purple-700 text-center text-white px-4 py-3 text-lg font-bold rounded-full hover:bg-[#872078] transition-colors duration-300 shadow-md"
+                                onClick={() => setIsOpen(false)}
+                            >
                                 Connexion
                             </Link>
-                        </li>
-                    </ul>
-                </div>
-            )}
-        </nav>
+                            </li>
+                        </ul>
+                    </div>
+                )
+            }
+          </div>
+        </nav >
     )
 }
 
 export default Header;
 
-
-
-
-
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom"; // pour la navigation
-// import { MdMenu, MdClose } from "react-icons/md";
-// import { motion, AnimatePresence } from "framer-motion";
-
-// function Header() {
-//     const [isOpen, setIsOpen] = useState(false);
-//     return (
-//         <nav className="bg-white fixed w-full flex justify-around py-2 gap-8 items-center shadow-custom ">
-//             <Link to="/">
-//                 <img src="/images/Noorwayslogo2.png" alt="NoorWays Logo" className="h-20" />
-//             </Link>
-//             {/* Menu de navigation */}
-//             <ul className="hidden md:flex space-x-9">
-//                 <li className="relative group">
-//                     <Link to="/" className="text-gray-700 text-2xl font-semibold px-1 py-2 ">
-//                         Accueil
-//                         <span className="absolute -left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-50 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
-//                     </Link>
-//                 </li>
-//                 {/* <li >
-//                     <Link to="/" className="text-gray-700 text-2xl font-semibold px-3  hover:py-2  hover:bg-[#db3de033] duration-300 rounded-lg">
-//                         Accueil
-//                     </Link>
-//                     <div className="border-b-4 w-7 border-yellow-200 "></div>
-//                 </li> */}
-//                 {/* <li>
-//                     <Link to="/" className="text-gray-700 text-2xl font-semibold px-3 py-2 hover:bg-[#db3de033] duration-300 rounded-lg">
-//                         Accueil
-//                     </Link>
-//                 </li> */}
-//                 <li className="relative group">
-//                     <Link to="/association" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
-//                         Associations
-//                         <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
-
-//                     </Link>
-//                 </li>
-//                 <li className="relative group">
-//                     <Link to="/benevole" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
-//                         Bénévoles
-//                         <span className="absolute left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
-
-//                     </Link>
-//                 </li>
-//                 <li className="relative group">
-//                     <Link to="/particulier" className="text-gray-700 text-2xl font-semibold  py-2  duration-300 ">
-
-//                         Particuliers
-//                         <span className="absolute left-0 top-9 w-full h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 rounded-full origin-left"></span>
-
-//                     </Link>
-//                 </li>
-
-//             </ul>
-
-//             <div className="md:ml-4 hidden md:block">
-//                 <Link to="/login" className="bg-[#18b710] text-white px-4 py-3 text-2xl font-bold rounded-full hover:bg-green-500  duration-500">
-//                     Connexion
-//                 </Link>
-//             </div>
-//             {/* Menu Mobile */}
-//             <button onClick={() => setIsOpen(!isOpen)} className="md:hidden text-gray-700 text-2xl focus:outline-none">
-//                 {isOpen ? <MdClose /> : <MdMenu />}
-//             </button>
-
-//             {/* Sidebar section */}
-
-
-//             <AnimatePresence>
-//                 {isOpen && (
-                   
-//                    <>
-//                     <motion.div
-//                         initial={{ opacity: 0, y: -100 }}
-//                         animate={{ opacity: 1, y: 0 }}
-//                         exit={{ opacity: 0 }}
-//                         transition={{ type: "spring", stiffness: 100 }}
-//                         className="absolute top-24 left-0 w-full  h-screen z-20 bg-[#872078] p-6"
-//                     >
-//                         <ul className="flex flex-col justify-center  pl-10 gap-6">
-//                             <li className="relative group" >
-//                                 <Link to="/" className=" text-3xl text-white font-semibold block  " onClick={() => setTimeout(() => setIsOpen(false), 300)}
-//                                 >
-//                                     Accueil
-//                                     <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e]  scale-x-0 group-hover:scale-x-100 transform duration-300 rounded-full origin-left"></span>
-//                                 </Link>
-//                             </li>
-//                             <li className="relative group" >
-//                                 <Link
-//                                     to="/association"
-//                                     className="text-3xl text-white font-semibold"
-//                                     onClick={() => setIsOpen(false)}>
-//                                     Associations
-//                                     <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100  duration-500 rounded-full origin-left"></span>
-
-//                                 </Link>
-//                             </li>
-//                             <li className="relative group" >
-//                                 <Link
-//                                     to="/benevole"
-//                                     className="text-3xl text-white font-semibold"
-//                                     onClick={() => setIsOpen(false)}>
-//                                     Bénévoles
-//                                     <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100  duration-500 rounded-full origin-left"></span>
-//                                 </Link>
-//                             </li>
-//                             <li className="relative group" >
-//                                 <Link
-//                                     to="/particulier"
-//                                     className="text-3xl text-white font-semibold"
-//                                     onClick={() => setIsOpen(false)}>
-//                                     Particuliers
-//                                     <span className="absolute left-0 top-9 w-full  h-1 bg-[#f7932e] transform scale-x-0 group-hover:scale-x-100  duration-500 rounded-full origin-left"></span>
-//                                 </Link>
-//                             </li>
-//                         </ul>
-//                    </motion.div>
-//                    <div> 
-//                     <Link
-//                         to="/login" className="bg-[#18b710] mt-5 block text-white px-4 py-3 text-2xl font-bold rounded-full hover:bg-green-500 duration-500"
-//                         onClick={() => setIsOpen(false)}>
-//                         Connexion
-//                     </Link>
-//                     </div>
-//                    </>
-//                 )}
-//             </AnimatePresence>
-
-//         </nav>
-//     );
-// }
-
-// export default Header;
